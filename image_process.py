9e4a83ed28d9036530bc4eef8ec5469178f31282
Koo <ninejy_lol@naver.com>
2020-11-30 17:09:28 +0900

    [2020-11-30] viewr 구현

--- image_process.py
+++ image_process.py
@@ -6,6 +6,7 @@ import pyrealsense2 as rs
 class ImageProcess:
     def __init__(self):
         self.depth_colormap = None
+        self.color_image = None
         self.pipeline = rs.pipeline()
         config = rs.config()
         config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
@@ -16,7 +17,11 @@ class ImageProcess:
     def shotting(self):
         frames = self.pipeline.wait_for_frames()
         depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
 
         depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
         depth_colormap = cv.applyColorMap(cv.convertScaleAbs(depth_image, alpha=0.03), cv.COLORMAP_JET)
         self.depth_colormap = np.swapaxes(depth_colormap, 0, 1)
+        self.color_image = np.swapaxes(color_image, 0, 1)
--- main.py
+++ main.py
@@ -11,11 +11,11 @@ class Viewer(tk.Frame):
         INPUT_POSITION_X = 10
         INPUT_POSITION_Y = 10
         INPUT_IMAGE_PLACE_Y = 30
-        IMAGE_WIDTH = 400
-        OUTPUT_PLACE_X = IMAGE_WIDTH + INPUT_POSITION_X
-        DEPTH_PLACE_X = 2*IMAGE_WIDTH + INPUT_POSITION_X
+        IMAGE_WIDTH = 500
+        OUTPUT_PLACE_X = 2*IMAGE_WIDTH + INPUT_POSITION_X
+        DEPTH_PLACE_X = IMAGE_WIDTH + INPUT_POSITION_X
         #button
-        BUTTON_PLACE_X = 3.5*IMAGE_WIDTH + INPUT_POSITION_X
+        BUTTON_PLACE_X = 3.2*IMAGE_WIDTH + INPUT_POSITION_X
 
         self.imgProc = ImageProcess()
         dobi = ImageTk.PhotoImage(Image.open('dobi.png'))
@@ -25,42 +25,46 @@ class Viewer(tk.Frame):
         input_text = tk.Label(text='Input Image', font=font)
         input_text.place(x=INPUT_POSITION_X, y=INPUT_POSITION_Y)
         self.input_image = tk.Label()
-        self.input_image.image = dobi
-        self.input_image.configure(image=dobi)
-        self.input_image.place(x=INPUT_POSITION_X, y=INPUT_IMAGE_PLACE_Y)
+        self.upload_image_to_tkinter(self.input_image, dobi, INPUT_POSITION_X, INPUT_IMAGE_PLACE_Y)
 
         # output image view
         ouput_text = tk.Label(text='Output Image', font=font)
         ouput_text.place(x=OUTPUT_PLACE_X, y=INPUT_POSITION_Y)
         self.ouput_image = tk.Label()
-        self.ouput_image.image = dobi
-        self.ouput_image.configure(image=dobi)
-        self.ouput_image.place(x=OUTPUT_PLACE_X, y=INPUT_IMAGE_PLACE_Y)
+        self.upload_image_to_tkinter(self.ouput_image, dobi, OUTPUT_PLACE_X, INPUT_IMAGE_PLACE_Y)
 
         # Depth Image view
         depth_text = tk.Label(text='Depth Image', font=font)
         depth_text.place(x=DEPTH_PLACE_X, y=INPUT_POSITION_Y)
         self.depth_image = tk.Label()
-        self.depth_image.image = dobi
-        self.depth_image.configure(image=dobi)
-        self.depth_image.place(x=DEPTH_PLACE_X, y=INPUT_IMAGE_PLACE_Y)
+        self.upload_image_to_tkinter(self.depth_image, dobi, DEPTH_PLACE_X, INPUT_IMAGE_PLACE_Y)
 
         # shooting button
         shooting_button = tk.Button(overrelief='solid', text='Shooting', command=self.shotting)
         shooting_button.place(x=BUTTON_PLACE_X, y=INPUT_POSITION_Y)
 
+    def upload_image_to_tkinter(self, label, img, *place):
+        axis = place
+        label.image= img
+        label.configure(image=img)
+        if axis != ():
+            label.place(x=axis[0], y=axis[1])
+
     def shotting(self):
         self.imgProc.shotting()
         depth_image_ndarray = self.imgProc.depth_colormap
         depth_image = ImageTk.PhotoImage(image=Image.fromarray(depth_image_ndarray))
-        self.depth_image.image = depth_image
-        self.depth_image.configure(image=depth_image)
+        self.upload_image_to_tkinter(self.depth_image, depth_image)
+
+        input_image_ndarray = self.imgProc.color_image
+        input_image = ImageTk.PhotoImage(image=Image.fromarray(input_image_ndarray))
+        self.upload_image_to_tkinter(self.input_image, input_image)
 
 
 def main():
     root = tk.Tk()
     root.title('졸업합시다')
-    root.geometry("1600x960+50+50")
+    root.geometry("1900x960+50+50")
     root.resizable(False, False)
     app = Viewer(root)
     root.mainloop()
